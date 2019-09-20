0fb5bf2ec1d5f92ef7bb0137e5986484671a5b14
Brandon O'Hara <brandon@brandonohara.com>
2019-09-19 20:59:20 -0600

    Create Setup Artisan Command

---
+++ app/Console/Commands/Setup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class Setup extends Command
+{
+    protected $signature = 'wuphf:setup';
+    protected $description = 'Install default users and seed database';
+
+    public function handle()
+    {
+        $this->info('Seeding the database.');
+        \Artisan::call('db:seed');
+        $this->info('WUPHF! WUPHF! WUPHF!');
+
+        $this->info('Creating Admin User');
+        $this->admin();
+        $this->info('WUPHF! WUPHF! WUPHF!');
+
+        $this->info('Successfully setup Wuphf.com! WUPHF!');
+    }
+
+    public function admin()
+    {
+        $password = $this->ask('Admin Password');
+
+        $user = User::where('email', 'ryan.howard@gmail.com')->first();
+
+        if ($user) {
+            $user->password = Hash::make($password);
+            $user->save();
+        }
+    }
+}
